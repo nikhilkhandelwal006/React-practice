4170d09aca96d84a368bfb73c78ff2e16e02fa0c
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2024-10-19 11:08:45 +0530

    Ch-5 JSX

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export const App=()=>{
+  return (
+    <div>
+    <div>
+      <img src="qot.jpg" alt="qot.jpg" width="30%" height="30%" />
+    </div>
+    <h2>Queen of tears</h2>
+    <h3>Rating: 8.2</h3>
+    <p>
+      Summary: Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt accusantium necessitatibus, praesentium ut sequi optio aliquid exercitationem ipsum cum, enim officiis animi quibusdam natus voluptatibus temporibus qui, tempora iste sed vitae rerum. Et!
+    </p>
+    </div>
+  )
+}
