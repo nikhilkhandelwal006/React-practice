4148d8e78e9eeda16a2aa89f91003e104301f4d7
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2024-10-19 11:25:00 +0530

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 export const App=()=>{
   return (
     <div>
-    <div>
-      <img src="qot.jpg" alt="qot.jpg" width="30%" height="30%" />
-    </div>
-    <h2>Queen of tears</h2>
-    <h3>Rating: 8.2</h3>
-    <p>
-      Summary: Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt accusantium necessitatibus, praesentium ut sequi optio aliquid exercitationem ipsum cum, enim officiis animi quibusdam natus voluptatibus temporibus qui, tempora iste sed vitae rerum. Et!
-    </p>
-    </div>
+   <NetflixSeries/>
+   <NetflixSeries/>
+   <NetflixSeries/>
+   <NetflixSeries/>
+
+   <NetflixSeries/>
+   </div>
   )
+}
+
+
+const NetflixSeries=()=>{
+  return (<div>
+  <div>
+    <img src="qot.jpg" alt="qot.jpg" width="30%" height="30%" />
+  </div>
+  <h2>Queen of tears</h2>
+  <h3>Rating: 8.2</h3>
+  <p>
+    Summary: Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt accusantium necessitatibus, praesentium ut sequi optio aliquid exercitationem ipsum cum, enim officiis animi quibusdam natus voluptatibus temporibus qui, tempora iste sed vitae rerum. Et!
+  </p>
+  </div>)
 }
