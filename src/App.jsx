563ca1665eb3469bc0ecccad97ce8f4c14963933
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2024-10-19 13:01:21 +0530

    Fragments & Dynamic Values

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-export const App=()=>{
+import React from "react";
+export const App = () => {
   return (
     <div>
-   <NetflixSeries/>
-   <NetflixSeries/>
-   <NetflixSeries/>
-   <NetflixSeries/>
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
 
-   <NetflixSeries/>
-   </div>
-  )
-}
+      <NetflixSeries />
+    </div>
+  );
+};
 
-
-const NetflixSeries=()=>{
-  return (<div>
-  <div>
-    <img src="qot.jpg" alt="qot.jpg" width="30%" height="30%" />
-  </div>
-  <h2>Queen of tears</h2>
-  <h3>Rating: 8.2</h3>
-  <p>
-    Summary: Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt accusantium necessitatibus, praesentium ut sequi optio aliquid exercitationem ipsum cum, enim officiis animi quibusdam natus voluptatibus temporibus qui, tempora iste sed vitae rerum. Et!
-  </p>
-  </div>)
-}
+const NetflixSeries = () => {
+  let name = "Queen of tears"
+ let  rating = 8.2
+ let  summary =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt accusantium necessitatibus, praesentium ut sequi optio aliquid exercitationem ipsum cum, enim officiis animi quibusdam natus voluptatibus temporibus qui, tempora iste sed vitae";
+    let Genre=()=>{
+      const Genre= "rom com"
+      return Genre
+    }
+  return (
+    <>
+      <div>
+        <img src="qot.jpg" alt="qot.jpg" width="30%" height="30%" />
+      </div>
+      <h2>{name}</h2>
+      <h3>Rating: {5+3.3}</h3>
+      <p>Summary: {summary}</p>
+      <p>Genre: {Genre()}</p>
+    </>
+  );
+};
