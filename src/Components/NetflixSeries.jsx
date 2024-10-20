5df4220e2ddae81c80d55cc160e9432072da1b0e
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2024-10-20 17:10:40 +0530

    props

--- src/Components/NetflixSeries.jsx
+++ src/Components/NetflixSeries.jsx
@@ -1,28 +1,9 @@
 import seriesData from "../api/seriesData.json";
+import {SeriesCard} from "./SeriesCard"
 const NetflixSeries = () => {
   return (
     <ul>
-      {seriesData.map((curElem) => {
-        return(
-        <li key={curElem.id}>
-          <div>
-            <img
-              src={curElem.img_url}
-              alt="qot.jpg"
-              width="30%"
-              height="30%"
-            />
-          </div>
-          <h2>{curElem.name}</h2>
-          <h3>Rating: {curElem.rating}</h3>
-          <p>Summary: {curElem.description}</p>
-          <p>Genre: {curElem.genre}</p>
-          <a href={curElem.watch_url} target="_blank">
-            <button>Watch Now</button>
-          </a>
-        </li>
-      );
-      })}
+      {seriesData.map((curElem) => <SeriesCard key={curElem.id} curElem={curElem}/>)}
     </ul>
   );
 };
