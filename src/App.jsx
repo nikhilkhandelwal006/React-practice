70f6861c7a801d92bf737d792d28021c5fc0d7e9
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2024-10-20 11:15:43 +0530

    map method

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import NetflixSeries,{Header,Footer} from "./Components/NetflixSeries";
+import NetflixSeries from "./Components/NetflixSeries";
 export const App = () => {
   return (
     <>
-    <Header/>
       <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <Footer/>
+      
     </>
   );
 };
