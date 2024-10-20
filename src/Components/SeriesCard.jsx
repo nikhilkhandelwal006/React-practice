406883beba510dd15fa396812ff24c45d2bb3e1c
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2024-10-20 17:44:13 +0530

    destructing props

--- src/Components/SeriesCard.jsx
+++ src/Components/SeriesCard.jsx
@@ -1,20 +1,22 @@
-export const SeriesCard=(props)=>{
-
+export const SeriesCard=({curElem})=>{
+console.log(props)
+// const {curElem}=props
+const {id,img_url, name, rating, description, cast, genre, watch_url}=props.curElem
 return(
-    <li key={props}>
+    <li >
       <div>
         <img
-          src={props.curElem.img_url}
-          alt={props.curElem.name}
+          src={img_url}
+          alt={name}
           width="30%"
           height="30%"
         />
       </div>
-      <h2>Name:{props.curElem.name}</h2>
-      <h3>Rating: {props.curElem.rating}</h3>
-      <p>Summary: {props.curElem.description}</p>
-      <p>Genre: {props.curElem.genre}</p>
-      <a href={props.curElem.watch_url} target="_blank">
+      <h2>Name:{name}</h2>
+      <h3>Rating: {rating}</h3>
+      <p>Summary: {description}</p>
+      <p>Genre: {genre}</p>
+      <a href={watch_url} target="_blank">
         <button>Watch Now</button>
       </a>
     </li>
