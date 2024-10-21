6f2f3fe33e9b9aab8f30c866779409f73093faed
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2024-10-21 13:17:25 +0530

    conditional styling

--- src/Components/SeriesCard.jsx
+++ src/Components/SeriesCard.jsx
@@ -8,9 +8,12 @@ export const SeriesCard = ({ curElem }) => {
     padding: "1.2rem 2.4rem",
     border: "none",
     fontSize: "1.6rem",
-    backgroundColor: "var(--bnt-hover-bg-color)",
+    backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
     color: "var(--bg-color)",
+    fontWeight:"bold",
+    cursor:"pointer"
   };
+  const ratingClass = rating >= 8.5 ? "super_hit" : "average";
   return (
     <li className="card">
       <div>
@@ -18,7 +21,9 @@ export const SeriesCard = ({ curElem }) => {
       </div>
       <div className="card-content">
         <h2>Name:{name}</h2>
-        <h3>Rating: {rating}</h3>
+        <h3>
+          Rating: <span className={`rating ${ratingClass}`}> {rating}</span>
+        </h3>
 
         <p>Summary: {description}</p>
 
