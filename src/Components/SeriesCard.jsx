27a93d2f87f66e9181814db4775ffd0fe08ff06d
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2024-10-21 18:19:02 +0530

    styled-components

--- src/Components/SeriesCard.jsx
+++ src/Components/SeriesCard.jsx
@@ -1,20 +1,37 @@
 import styles from "./Netflix.module.css";
+import styled from "styled-components";
+
 export const SeriesCard = ({ curElem }) => {
   // console.log(props)
   // const {curElem}=props
   const { id, img_url, name, rating, description, cast, genre, watch_url } =
     curElem;
 
-  const btn_style = {
-    padding: "1.2rem 2.4rem",
-    border: "none",
-    fontSize: "1.6rem",
-    backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
-    color: "var(--bg-color)",
-    fontWeight:"bold",
-    cursor:"pointer"
-  };
+  // const btn_style = {};
+
+  //  const ButtonNik=styled.button({
+  //   padding: "1.2rem 2.4rem",
+  //   border: "none",
+  //   fontSize: "1.6rem",
+  //   backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
+  //   color: "var(--bg-color)",
+  //   fontWeight: "bold",
+  //   cursor: "pointer",
+  //  })
+
+  const ButtonNik = styled.button`
+    padding: 1.2rem 2.4rem;
+    border: none;
+    font-Size: 1.6rem;
+    background-Color: ${(props) =>
+      props.rating >= 8.5 ? "#7dcea0" : "#f7dc6f"};
+    color: var(--bg-color);
+    font-Weight: bold;
+    cursor: pointer;
+   `;
+
   const ratingClass = rating >= 8.5 ? styles.super_hit : styles.average;
+
   return (
     <li className={styles.card}>
       <div>
@@ -23,7 +40,8 @@ export const SeriesCard = ({ curElem }) => {
       <div className={styles["card-content"]}>
         <h2>Name:{name}</h2>
         <h3>
-          Rating: <span className={`${styles.rating} ${ratingClass}`}> {rating}</span>
+          Rating:{" "}
+          <span className={`${styles.rating} ${ratingClass}`}> {rating}</span>
         </h3>
 
         <p>Summary: {description}</p>
@@ -31,7 +49,9 @@ export const SeriesCard = ({ curElem }) => {
         <p>Genre: {genre.join(",")}</p>
         <p>Cast: {cast.join(",")}</p>
         <a href={watch_url} target="_blank">
-          <button style={btn_style}>Watch Now</button>
+          {/* <button style={btn_style}>Watch Now</button> */}
+
+          <ButtonNik rating={rating}>Watch Now</ButtonNik>
         </a>
       </div>
     </li>
